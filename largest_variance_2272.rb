45879c6b20f2a3ebf0936ec30f3af489854c0e4d
Aaron Burrow <burrows@fastmail.com>
2023-07-08 23:12:30 -0400

    test cases

--- largest_variance_2272.rb
+++ largest_variance_2272.rb
@@ -41,6 +41,32 @@ def variance_bf(s)
     xs[-1] - xs[0]
 end
 
+def largest_variance(s)
+    if !(s.is_a?(String) and 1 <= s.size)
+        raise "invalid input"
+    end
+
+    start = 0
+    endd = s.size - 1
+end
+
+def randstring(chars, k)
+    if !(chars.is_a?(Array) and 1 <= chars.size)
+        raise "invalid input"
+    elsif !(k.is_a?(Integer) and 0 <= k)
+        raise "invalid input"
+    end
+
+    xs = []
+    i = 0
+    while i < k
+        xs.append(chars[rand(chars.size)])
+        i += 1
+    end
+    xs
+end
+
+
 raise "Hell" if variance_bf("a") != 0
 raise "Hell" if variance_bf("aa") != 0
 raise "Hell" if variance_bf("ab") != 0
@@ -54,3 +80,15 @@ raise "Hell" if variance_bf("aabc") != 1
 
 raise "Hell" if largest_variance_bf("aababbb") != 3
 raise "Hell" if largest_variance_bf("abcde") != 0
+
+# This is a counter example to naive two pointer algorithm.
+raise "Hell" if largest_variance_bf("aaaacbdcdcbc") != 3
+
+# Interesting case where the most frequent character in the entire string
+# is not the most frequent character in the highest variance substring.
+raise "Hell" if largest_variance_bf("aaaaabbbbbbbaaaaa") != 6
+
+pp randstring(["a","b"],7)
+pp randstring(["a","b","c"],10)
+pp randstring(["a","b","c"],10)
+pp randstring(["a","b","c"],10)
